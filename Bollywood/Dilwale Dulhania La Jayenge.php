0ae771324ad041b9ee400703c2871972d7c2aedb
don24crk <45152810+don24crk@users.noreply.github.com>
2024-01-14 19:07:18 +0100

    Add files via upload
    bollywood app

---
+++ Bollywood/Dilwale Dulhania La Jayenge.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>ERT1</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="Description" lang="en" content="">
+<meta name="author" content="">
+<meta name="robots" content="index, follow">
+<link rel="stylesheet" href="assets/css/styles.css">
+<script src="//code.jquery.com/jquery-3.3.1.min.js"></script>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script src="https://www.google.com/recaptcha/api.js?render=6LfEjcsUAAAAAD2eNDV82V9ux_5oyKmkiLwzmTYW"></script>
+<script>
+    grecaptcha.ready(function() {
+        grecaptcha.execute('6LfEjcsUAAAAAD2eNDV82V9ux_5oyKmkiLwzmTYW', {action:'validate_captcha'}).then(function(token) {
+            document.getElementById('g-recaptcha-response').value = token;
+        });
+    });
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-5487194871056744",
+    enable_page_level_ads: true
+  });
+</script>
+</head>
+<body>
+<div class="container">
+<center>
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="2DEMRNWQR5GNC">
+<input type="image" src="http://www.anacon.org/app/chans/dorean.jpg" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+</center>
+<div class="content">
+<center>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Lakatamia Auto Ads -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-5487194871056744"
+     data-ad-slot="4552784315"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+</center>
+<div class="main">
+
+<form id="form_id" method="post" action="https://delivery-node-cxs4etrluyvcqo5w.voe-network.net/engine/hls2-c/01/05331/ey5mfj2ihzg2_,n,.urlset/master.m3u8?t=xaUjFkNgmWuk0J08f2TlDQDIciWsG0qsL6fPV9ug-LI&s=1705253684&e=14400&f=26655123&node=delivery-node-fhttlmsj2uektk7t.voe-network.net&i=91.0&sp=2500&asn=3320">
+    <input type="hidden" id="g-recaptcha-response" name="g-recaptcha-response">
+    <input type="hidden" name="action" value="validate_captcha">
+    <input type="image" src="/app/chans/gr/exo.jpg" alt="" style="width:100%;" />
+</form>
+
+</div>
+<center>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Lakatamia Auto Ads -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-5487194871056744"
+     data-ad-slot="4552784315"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+</center>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-40514048-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</div>
+</div>
+</body>
+</html>
